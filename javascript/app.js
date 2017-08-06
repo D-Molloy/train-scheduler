37be38388541732efb99d3aaef4b33117e0deb10
Denis Molloy <denis.molloy@gmail.com>
2017-08-06 13:57:15 -0400

    updating files

--- javascript/app.js
+++ javascript/app.js
@@ -1,78 +1,77 @@
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyARc6FL06wzs1-SRu6XCUW9nbyLdRgXHkw",
-    authDomain: "train-scheduler-7e066.firebaseapp.com",
-    databaseURL: "https://train-scheduler-7e066.firebaseio.com",
-    projectId: "train-scheduler-7e066",
-    storageBucket: "",
-    messagingSenderId: "336436315525"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyARc6FL06wzs1-SRu6XCUW9nbyLdRgXHkw",
+authDomain: "train-scheduler-7e066.firebaseapp.com",
+databaseURL: "https://train-scheduler-7e066.firebaseio.com",
+projectId: "train-scheduler-7e066",
+storageBucket: "",
+messagingSenderId: "336436315525"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+//create a local instance of the firevase
+var database = firebase.database();
 
-  $('#add-train').on('click', function(){
-  	event.preventDefault();
+//event handler for clicking on the Submit button
+$('#add-train').on('click', function(){
+	event.preventDefault();
 
-  	var name = $("#name-input").val().trim();
-  	var dest = $("#dest-input").val().trim();
-  	var startTime = $("#start-input").val().trim();
-  	var freq = $("#freq-input").val().trim();
+	//get the values form the form fields and store them in variables
+	var name = $("#name-input").val().trim();
+	var dest = $("#dest-input").val().trim();
+	var startTime = $("#start-input").val().trim();
+	var freq = $("#freq-input").val().trim();
 
-  	database.ref().push({
-            name: name,
-            dest: dest,
-            startTime: startTime,
-            freq: freq,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
-
-  	});
-
-    database.ref().on("child_added", function (childSnapshot) {
-
-        // calculate months worked
-        var curTime = moment().format('HH:mm');
-        console.log("current time: "+curTime);
- 
-        var trainStart = childSnapshot.val().startTime
-        console.log("inputted start time: " + trainStart)
-        
-    	var timeDif = Number(curTime) - Number(trainStart);
-    	console.log(timeDif);
-
-        //1- get current time (military)
-
-        //2- get train start (military)
-
-        //3- subtract train start from current time to get the difference  (not sure what to do about minutes)
-
-        //4- multiply the difference * 60 to get the total number of minutes (totalMinutes) 
-
-        //5- divide totalMinutes by the train frequency (minuteDif)
-
-        //6- subtract Math.floor(minuteDif) from 60 to get Minutes Away
-
-        //use the add method to add the minutes away to current time: moment().add(minutesAway, 'm')
-
-        //???
-
-        //profit
-
-
-        var nextArrival = "Soon"
-        var minAway = "a couple"
-
-        // add new row to on-screen table
-        $("#emp-table").append(
-        	"<tr><td>" + childSnapshot.val().name + "</td>" +
-            "<td>" + childSnapshot.val().dest + "</td>" +
-            "<td>" + childSnapshot.val().freq + "</td>" +
-            "<td>" + nextArrival + "</td>" +
-            "<td>" + minAway + "</td></tr>");
-        		// Number(months) * Number(childSnapshot.val().rate) 
-        // Handle the errors
-    }, function (errorObject) {
-        console.log("Errors handled: " + errorObject.code);
+	//push them as children to the firebase
+	database.ref().push({
+        name: name,
+        dest: dest,
+        startTime: startTime,
+        freq: freq,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
+
+	//clear the values from the input fields
+	$("#name-input").val("");
+	$("#dest-input").val("");
+	$("#start-input").val("");
+	$("#freq-input").val("");
+
+});
+
+// print out the info in the database to the Current Train Schedule whenever the page loads or a new record is added
+database.ref().on("child_added", function (childSnapshot) {
+ 
+    //get current time
+    var curTime = moment();
+    
+    //get train start from the database
+    var trainStart = childSnapshot.val().startTime
+
+   	// Pushing the start time back 1 yr to ensure it comes before the current time
+   	var trainStartConv = moment(trainStart, "hh:mm").subtract(1, "years");
+
+    // calculate the difference between trainStart and curTime
+    var diffTime = moment().diff(moment(trainStartConv), "minutes");
+    
+    // time apart
+  	var timeApart = diffTime % childSnapshot.val().freq;
+
+  	// minutes until arrival
+  	var minTillTrain = childSnapshot.val().freq - timeApart;
+
+   	//adding minutes until the train to the current time and formatting the appearance of the time
+   	var nextArrival = moment().add(minTillTrain, 'm').format('LT');
+
+    // add new row to on-screen table
+    $("#emp-table").append(
+    	"<tr><td>" + childSnapshot.val().name + "</td>" +
+        "<td>" + childSnapshot.val().dest + "</td>" +
+        "<td>" + childSnapshot.val().freq + "</td>" +
+        "<td>" + nextArrival + "</td>" +
+        "<td>" + minTillTrain + "</td></tr>");
+   // Handle the errors
+	}, function (errorObject) {
+    	console.log("Errors handled: " + errorObject.code);
+});
 
